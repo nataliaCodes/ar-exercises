84161fd13965a492bc1d0bb2752a2c6e980c5538
Natalia Martian <natalia.martian@gmail.com>
2021-01-05 03:40:27 +0000

    Solve ex 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+
+@store1 = Store.first
+
+@store2 = Store.find_by(id: 2)
+puts @store2.name
+@store2.name = 'Winners'
+puts @store2.name
